4438a6f1ca439f18ce1d07fd3d4b3c63793cacdd
SivaPrinda <prinda.sivakumaran@gmail.com>
2024-10-03 22:09:15 +0200

    Initial commit for Java project

---
+++ src/main/java/com/openclassrooms/P3_API_Portail_locataire/P3ApiPortailLocataireApplication.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.P3_API_Portail_locataire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P3ApiPortailLocataireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P3ApiPortailLocataireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openclassrooms/P3_API_Portail_locataire/P3ApiPortailLocataireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassrooms.P3_API_Portail_locataire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P3ApiPortailLocataireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
